22615802aba2a3ceb5756c81f6203076804205f0
qx <iqx7@foxmail.com>
2023-07-18 09:26:53 +0800

    停止游戏

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const sendDingTalk = require('./src/sendDingTalk');
 const sendWxWork = require('./src/sendWxWork')
 const getPoint = require('./src/getPoint');
 
-const { autoGame } = require('./src/games/autoRun');
+// const { autoGame } = require('./src/games/autoRun');
 
 (async () => {
   // 上次分数
@@ -33,12 +33,12 @@ const { autoGame } = require('./src/games/autoRun');
 
   console.log(draw_res);
 
-  let game_res = '挖矿成功！';
-  try {
-    await autoGame();
-  } catch (error) {
-    game_res = '挖矿失败！';
-  }
+  // let game_res = '挖矿成功！';
+  // try {
+  //    await autoGame();
+  // } catch (error) {
+  //   game_res = '挖矿失败！';
+  // }
 
   // 当前分数
   const now_score = await getPoint();
@@ -61,8 +61,7 @@ const { autoGame } = require('./src/games/autoRun');
       <p style="text-indent: 2em">当前矿石：${now_score}</p>
       <p style="text-indent: 2em">较昨日增长：${now_score - yesterday_score}</p>
       <p style="text-indent: 2em">签到结果：${sign_res}</p>
-      <p style="text-indent: 2em">抽奖结果：${draw_res}</p>
-      <p style="text-indent: 2em">游戏结果：${game_res}</p><br/>
+      <p style="text-indent: 2em">抽奖结果：${draw_res}</p><br/>
     `;
 
     console.log(html);
@@ -81,7 +80,6 @@ const { autoGame } = require('./src/games/autoRun');
       较昨日增长：${now_score - yesterday_score}
       签到结果：${sign_res}
       抽奖结果：${draw_res}
-      游戏结果：${game_res}
     `;
 
     await sendDingTalk(msg);
@@ -92,7 +90,7 @@ const { autoGame } = require('./src/games/autoRun');
   }
 
   try {
-    const html = `掘金自动签到通知\n> 沾喜气结果:<font color=\"comment\">${dip_res}</font>\n> 当前矿石:<font color=\"comment\">${now_score}</font>\n> 较昨日增长:<font color=\"comment\">${now_score - yesterday_score}</font>\n>签到结果:<font color=\"comment\">${sign_res}</font>\n> 抽奖结果:<font color=\"comment\">${draw_res}</font>\n> 游戏结果:<font color=\"comment\">${game_res}</font>`
+    const html = `掘金自动签到通知\n> 沾喜气结果:<font color=\"comment\">${dip_res}</font>\n> 当前矿石:<font color=\"comment\">${now_score}</font>\n> 较昨日增长:<font color=\"comment\">${now_score - yesterday_score}</font>\n>签到结果:<font color=\"comment\">${sign_res}</font>\n> 抽奖结果:<font color=\"comment\">${draw_res}</font>\n>`
 
     const msg = await sendWxWork(html)
 
